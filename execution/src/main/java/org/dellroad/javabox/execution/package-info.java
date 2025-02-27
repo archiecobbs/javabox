d1aa7fa53b0a575a536e03c62b6daf3ef98ab1a6
Archie L. Cobbs <archie.cobbs@gmail.com>
2025-02-27 10:40:30 -0600

    Refactor Maven project to have "main" and "execution" modules.

---
+++ execution/src/main/java/org/dellroad/javabox/execution/package-info.java
@@ -0,0 +1,9 @@
+
+/*
+ * Copyright (C) 2025 Archie L. Cobbs. All rights reserved.
+ */
+
+/**
+ * Classes used by <a href="https://github.com/archiecobbs/javabox">JavaBox</a> for local execution in JShell.
+ */
+package org.dellroad.javabox.execution;
